aabf223868ac264a3e17db9d140a259e3ba15580
Maksym <148781411+maksym-sk21@users.noreply.github.com>
2025-01-22 11:29:54 +0200

    Delete turnover directory

--- turnover/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class TurnoverConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'turnover'
-
-    def ready(self):
-        import turnover.signals
