05177402ba58bc48e9e7f655ec717956e305e636
Maksym <148781411+maksym-sk21@users.noreply.github.com>
2025-01-22 11:29:11 +0200

    Delete messaging directory

--- messaging/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'messaging'
-
-urlpatterns = [
-    path('conversations/', views.conversation_list, name='conversation_list'),
-    path('conversations/<int:conversation_id>/', views.conversation_view, name='conversation_view'),
-]
