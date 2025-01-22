9054cdbe640756f9021262f3c099fbc70847941b
Maksym <148781411+maksym-sk21@users.noreply.github.com>
2025-01-22 11:29:18 +0200

    Delete order directory

--- order/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from students.models import Student
-
-# Create your models here.
-class Order(models.Model):
-    order_id = models.CharField(max_length=255, unique=True)
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    lesson_count = models.IntegerField()
-    status = models.CharField(max_length=50, default='pending')
